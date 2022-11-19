1e5db9573f985e1dd9f6e8395081c45fc38531af
Samuel Emeka <samuelemeka2021@outlook.com>
2022-11-19 00:15:14 -0600

    editted ts

--- src/app/my-threejs/my-threejs.component.ts
+++ src/app/my-threejs/my-threejs.component.ts
@@ -180,8 +180,8 @@ export class MyThreejsComponent implements OnInit {
       // Update objects
       sphere.rotation.y = .5 * elapsedTime
       sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
-      sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
-      sphere.position.z += -.05 * (targetY - sphere.rotation.x)
+      sphere.rotation.x += .5 * (targetY - sphere.rotation.x)
+      sphere.position.z += -.5 * (targetY - sphere.rotation.x)
 
       // Update Orbital Controls
       // controls.update()
